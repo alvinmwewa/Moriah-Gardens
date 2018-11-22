1d83b8da41a48dafab4296f86cf7923e77824ee5
Alvin <mwewamwewa1699@gmail.com>
2018-11-22 21:14:59 +0200

    worked on about and css as well as new features

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -19,32 +19,32 @@ class Home extends Component {
   </form>
 </div>
 <div class="parallax">
-  <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
-    {/* <!-- Indicators --> */}
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-	  <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
+  {/* <div id="myCarousel" class="carousel slide text-center" data-ride="carousel">
+    /* <!-- Indicators --> */
+    // <ol class="carousel-indicators">
+    //   <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    //   <li data-target="#myCarousel" data-slide-to="1"></li>
+    //   <li data-target="#myCarousel" data-slide-to="2"></li>
+	  // <li data-target="#myCarousel" data-slide-to="3"></li>
+    // </ol>
 
-    {/* <!-- Wrapper for slides --> */}
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <h4>"This company is the best. I am so happy with the result!"<br/><span>Michael Roe, Vice President, Comment Box</span></h4>
-      </div>
-      <div class="item">
-        <h4>"One word... WOW!!"<br/><span>John Doe, Salesman, Rep Inc</span></h4>
-      </div>
-      <div class="item">
-        <h4>"Could I... BE any more happy with this company?"<br/><span>Chandler Bing, Actor, FriendsAlot</span></h4>
-      </div>
-	  <div class="item">
-        <h4>"this is good stuff i tell you.............."<br/><span>Alvin Mwewa, Programmer, devoloper</span></h4>
-      </div>
-    </div>
+    /* <!-- Wrapper for slides --> */
+    // <div class="carousel-inner" role="listbox">
+    //   <div class="item active">
+    //     <h4>"This company is the best. I am so happy with the result!"<br/><span>Michael Roe, Vice President, Comment Box</span></h4>
+    //   </div>
+    //   <div class="item">
+    //     <h4>"One word... WOW!!"<br/><span>John Doe, Salesman, Rep Inc</span></h4>
+    //   </div>
+    //   <div class="item">
+    //     <h4>"Could I... BE any more happy with this company?"<br/><span>Chandler Bing, Actor, FriendsAlot</span></h4>
+    //   </div>
+	  // <div class="item">
+    //     <h4>"this is good stuff i tell you.............."<br/><span>Alvin Mwewa, Programmer, devoloper</span></h4>
+    //   </div>
+    // </div>
 
-    {/* <!-- Left and right controls --> */}
+    /* <!-- Left and right controls --> *
     <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
       <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
       <span class="sr-only">Previous</span>
@@ -53,7 +53,7 @@ class Home extends Component {
       <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
       <span class="sr-only">Next</span>
     </a>
-  </div>
+  </div> */}
   </div>
   
   {/* <!-- Container (Contact Section) --> */}
